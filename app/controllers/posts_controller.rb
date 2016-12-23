f635ea9098c3fe081ea1e539ec88a4ad4dd0d43d
Alireza Andersen <alireza.andersen@gmail.com>
2016-12-22 18:09:17 -0700

    added index and new to post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,23 @@
 class PostsController < ApplicationController
   before_action :find_post, only[:edit, :update, :show, :delete]
 
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+
   def find_post
     @post = Post.find(params[:id])
   end
+
 end
