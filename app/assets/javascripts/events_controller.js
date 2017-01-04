f19f4d3851e607718f88b4df7b540f0bccf07463
Alireza Andersen <alireza.andersen@gmail.com>
2017-01-03 23:49:16 -0700

    SO LOST WTF! VIEWS

--- app/assets/javascripts/events_controller.js
+++ app/assets/javascripts/events_controller.js
@@ -1,6 +1,6 @@
 function openPopup(link)
 {
     link.hide();
-    window.open(link.href,'event_path'event,'toolbar=no,location=no,menubar=no,scrollbars=yes,resizable=no');
+    // window.open(link.href,'event_path')event,'toolbar=no,location=no,menubar=no,scrollbars=yes,resizable=no');
     return false;
 }
